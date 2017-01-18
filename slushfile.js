816b51821f5c76e63a839ddff0cea87462d5f6a2
angeltveit <torbjorn@skjermpost.no>
2017-01-18 23:28:32 +0100

    explain

--- slushfile.js
+++ slushfile.js
@@ -5,18 +5,37 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     inquirer = require('inquirer'),
     sequence = require('run-sequence');
-
+    config = require('./config.json')
 var answers = null
+
+var prompts = [{
+      type: 'input',
+      name: 'modelName',
+      message: 'Name of your new model'
+    },{
+      type: 'input',
+      name: 'middlewareCreate',
+      message: 'Middleware for creating(restrict access etc)'
+    },{
+      type: 'input',
+      name: 'middlewareRead',
+      message: 'Middleware for reading(restrict access etc)'
+    },{
+      type: 'input',
+      name: 'middlewareUpdate',
+      message: 'Middleware for updating(restrict access etc)'
+    },{
+      type: 'input',
+      name: 'middlewareDelete',
+      message: 'Middleware for deleting(restrict access etc)'
+    }]
+
 gulp.task('default', function (done) {
-  inquirer.prompt([
-    {type: 'input', name: 'modelName', message: 'Name of your new model', default: gulp.args.join(' ')}, // Get app name from arguments by default
-    {type: 'confirm', name: 'moveon', message: 'Continue?'}
-  ],
-  function (ans) {
-    if (!ans.moveon) {
-      return done()
-    }
-    answers = ans
+  inquirer.prompt(prompts,
+  function (results) {
+    answers = results
+    answers.modelName =
+      answers.modelName.charAt(0).toUpperCase() + answers.modelName.slice(1)
     sequence('models','crud')
   });
 });
@@ -26,17 +45,18 @@ gulp.task('models', function(done) {
     .pipe(template(answers))
     .pipe(rename('index.js'))
     .pipe(conflict('./'))
-    .pipe(gulp.dest(`./models/${answers.modelName.toLowerCase()}/`))
+    .pipe(gulp.dest(`${config.modelsDir}/${answers.modelName.toLowerCase()}/`))
     .on('finish', function () {
       done();
     });
 })
 
 gulp.task('crud', function(done) {
+
   gulp.src(__dirname + '/templates/crud/**.js')
     .pipe(template(answers))
     .pipe(conflict('./'))
-    .pipe(gulp.dest(`./routes/api/${answers.modelName.toLowerCase()}/`))
+    .pipe(gulp.dest(`${config.apiDir}${answers.modelName.toLowerCase()}/`))
     .on('finish', function () {
       done();
     });
--- templates/crud/index.js
+++ templates/crud/index.js
@@ -11,7 +11,7 @@ function index(req, res, next) {
 
 module.exports = require('express').Router()
   .get('/', index)
-  .get('/:id', require('./read'))
-  .post('/', require('./create'))
-  .put('/:id',require('./update'))
-  .delete('/:id', require('./delete'))
+  .get('/:id', <% if(middlewareRead){%>[<%= middlewareRead %>],<%}%> require('./read'))
+  .post('/', <% if(middlewareCreate){%>[<%= middlewareCreate %>],<%}%> require('./create'))
+  .put('/:id', <% if(middlewareUpdate){%>[<%= middlewareUpdate %>],<%}%> require('./update'))
+  .delete('/:id', <% if(middlewareDelete){%>[<%= middlewareDelete %>],<%}%> require('./delete'))
