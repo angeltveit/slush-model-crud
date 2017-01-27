089b42ab5c52b32854ad7331da5a930d5a5cdb07
Torbjorn Angeltveit <torbjorn@globalit.no>
2017-01-27 16:55:27 +0100

    removed useless files

--- routes/api/project/read.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req, res, next) {
-  var Project = req.model('Project')
-  Project.findOne({_id: req.params.id})
-    .then(function(data){
-      res.json(data)
-    })
-}
